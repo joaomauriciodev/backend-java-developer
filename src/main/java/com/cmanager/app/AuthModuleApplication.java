92988de48153eb3121df6ef6681615abd04137fe
João Mauricio <joaomauricio.dev@gmail.com>
2026-04-15 07:48:13 -0300

    feat(cache): Enable caching in AuthModuleApplication

--- src/main/java/com/cmanager/app/AuthModuleApplication.java
+++ src/main/java/com/cmanager/app/AuthModuleApplication.java
@@ -2,8 +2,10 @@ package com.cmanager.app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
+@EnableCaching
 public class AuthModuleApplication {
     public static void main(String[] args) {
         SpringApplication.run(AuthModuleApplication.class, args);
